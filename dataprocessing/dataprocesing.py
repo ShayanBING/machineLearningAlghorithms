05cf5af589fb0c8433c8ac55c974195ef891acb4
shayanBING <shafieazad.shayan@gmail.com>
2020-06-01 13:43:46 +0430

    splite train set and test set from our data set

--- dataprocessing/dataprocesing.py
+++ dataprocessing/dataprocesing.py
@@ -32,8 +32,7 @@ le =LabelEncoder()
 y = le.fit_transform(y)
 
 #Splitting the dataset into the Training set and Test set
-
-
-
+from sklearn.model_selection import train_test_split
+x_train , x_test , y_train , y_test = train_test_split(x,y,test_size=0.2,random_state=1)
 #Feature Scaling
 
