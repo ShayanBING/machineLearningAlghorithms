f501e348a394e1b847630c613e7aebb8b98e509e
shayanBING <shafieazad.shayan@gmail.com>
2020-06-08 16:02:02 +0430

    Test All Regression Models With Real Example For Model Selection

--- regressionModelSelection/svr.py
+++ regressionModelSelection/svr.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('./Data.csv')
-x = dataset.iloc[:, 1:-1].values
+x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 y = y.reshape(len(y),1)
